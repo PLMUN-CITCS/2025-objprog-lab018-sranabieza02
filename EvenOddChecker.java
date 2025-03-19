9d70d56789eeaca3eb1ed629f340a8743150c50a
shiena rose anabieza <anabiezashienarose_bsit@plmun.edu.ph>
2025-03-20 01:34:15 +0800

    Submitting OBJPROG Week 05 - Laboratory # 18

---
+++ EvenOddChecker.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class EvenOddChecker {
+
+    public static int getIntegerInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        
+        while (!scanner.hasNextInt()) {
+            System.out.println("Invalid input. Please enter a valid integer.");
+            scanner.next(); // consume the invalid input
+            System.out.print("Enter an integer: ");
+        }
+        
+        return scanner.nextInt();
+    }
+
+    public static String checkEvenOrOdd(int number) {
+        if (number % 2 == 0) {
+            return number + " is an Even number.";
+        } else {
+            return number + " is an Odd number.";
+        }
+    }
+
+    public static void main(String[] args) {
+        int userInput = getIntegerInput();
+        
+        String result = checkEvenOrOdd(userInput);
+        
+        System.out.println(result);
+    }
+}
